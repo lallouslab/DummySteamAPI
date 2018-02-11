e1f1146ab098952b946d4cba8b61006e247d329a
Guillaume Charifi <guillaume.charifi@sfr.fr>
2018-02-11 23:22:48 +0100

    os/linux: Implement dsa_os_mkdir().

--- src/os/linux/linux.c
+++ src/os/linux/linux.c
@@ -1,7 +1,14 @@
+#include <sys/stat.h> /* mkdir() */
+
 #include "utils.h"
 #include "os/os.h"
 
 EXPORT int dsa_os_init()
 {
 	return 0;
+}
+
+EXPORT int dsa_os_mkdir(const char *path)
+{
+	return mkdir(path, 0777);
 }
--- src/os/os.h
+++ src/os/os.h
@@ -2,5 +2,6 @@
 #define OS_OS_H 1
 
 extern int dsa_os_init();
+extern int dsa_os_mkdir(const char *path);
 
 #endif /* OS_OS_H */
