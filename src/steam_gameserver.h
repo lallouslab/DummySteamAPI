da48fbab9ba079f4f5544f0c0f791d42f4612128
Guillaume Charifi <guillaume.charifi@sfr.fr>
2018-04-04 03:42:13 +0200

    steam_gameserver: Export the functions to header file.

--- src/steam_gameserver.h
+++ src/steam_gameserver.h
@@ -13,4 +13,15 @@ enum steam_game_server_mode
 
 extern struct ISteamClient *g_pSteamClientGameServer;
 
+extern steam_bool_t SteamGameServer_Init(uint32_t ip, uint16_t steam_port, uint16_t game_port, uint16_t query_port, enum steam_game_server_mode server_mode, const char *version);
+extern steam_bool_t SteamInternal_GameServer_Init(uint32_t ip, uint16_t steam_port, uint16_t game_port, uint16_t query_port, enum steam_game_server_mode server_mode, const char *version);
+extern steam_bool_t SteamGameServer_InitSafe(uint32_t ip, uint16_t steam_port, uint16_t game_port, uint16_t query_port, enum steam_game_server_mode server_mode, const char *version);
+extern uint32_t SteamGameServer_GetIPCCallCount(void);
+extern steam_handle_pipe_t SteamGameServer_GetHSteamPipe(void);
+extern steam_user_t SteamGameServer_GetHSteamUser(void);
+extern steam_bool_t SteamGameServer_BSecure(void);
+extern union CSteamID SteamGameServer_GetSteamID(void);
+extern void SteamGameServer_Shutdown(void);
+extern void SteamGameServer_RunCallbacks(void);
+
 #endif /* STEAM_GAMESERVER_H */
